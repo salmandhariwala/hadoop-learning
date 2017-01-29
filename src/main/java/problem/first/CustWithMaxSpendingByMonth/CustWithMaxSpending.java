f32eff2c522b3ca05cc3469af6402ee8da75fc00
salman <salmandhariwala@gmail.com>
2017-01-29 14:33:50 +0530

    added some comments

--- src/main/java/problem/first/CustWithMaxSpendingByMonth/CustWithMaxSpending.java
+++ src/main/java/problem/first/CustWithMaxSpendingByMonth/CustWithMaxSpending.java
@@ -18,74 +18,204 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class CustWithMaxSpending {
 	public static void main(String[] args) throws Exception {
+		/*
+		 * declare config object
+		 */
 		Configuration conf = new Configuration();
+
+		/*
+		 * declare job object
+		 */
 		Job job = Job.getInstance(conf, "CustWithMaxSpending");
 
+		/*
+		 * set main class for this job
+		 */
 		job.setJarByClass(CustWithMaxSpending.class);
-		job.setInputFormatClass(TextInputFormat.class);
-		// job.setMapperClass(MaxSpendMapper.class);
 
+		/*
+		 * set input file type for this job
+		 */
+		job.setInputFormatClass(TextInputFormat.class);
+
+		/*
+		 * define input paths along with their data type and their mapper
+		 */
 		MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, MaxSpendMapper.class);
 		MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, MaxSpendMapper.class);
 		MultipleInputs.addInputPath(job, new Path(args[2]), TextInputFormat.class, MaxSpendMapper.class);
 		MultipleInputs.addInputPath(job, new Path(args[3]), TextInputFormat.class, MaxSpendMapper.class);
 
+		/*
+		 * set mapper output key and value
+		 */
 		job.setMapOutputKeyClass(TextPair.class);
 		job.setMapOutputValueClass(LongWritable.class);
 
+		/*
+		 * set number of reducer
+		 */
 		job.setNumReduceTasks(4);
+
+		/*
+		 * Define the comparator that controls how the keys are sorted before
+		 * they are passed to the Reducer.
+		 */
 		job.setSortComparatorClass(SortComparator.class);
+
+		/*
+		 * Define the comparator that controls which keys are grouped together
+		 * for a single call to Reducer
+		 * 
+		 */
 		job.setGroupingComparatorClass(GroupingComparator.class);
 
+		/*
+		 * set partitioner of job
+		 * 
+		 * partitionar will chose which reducer to send data
+		 */
 		job.setPartitionerClass(MaxSpendPartitioner.class);
+
+		/*
+		 * set reducer for this job
+		 */
 		job.setReducerClass(MaxSpendReducer.class);
 
+		/*
+		 * set output key data type and value datatype
+		 */
 		job.setOutputKeyClass(LongWritable.class);
 		job.setOutputValueClass(Text.class);
 
-		// File Output Format
+		/*
+		 * define output directory
+		 */
 		FileOutputFormat.setOutputPath(job, new Path(args[4]));
 
+		/*
+		 * await termmination for this job
+		 */
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
 }
 
+/*
+ * param 1 : not known
+ * 
+ * param 2 : input line from the file
+ * 
+ * param 3 : ouput key
+ * 
+ * param 4 : output value
+ * 
+ * 
+ */
 class MaxSpendMapper extends Mapper<LongWritable, Text, TextPair, LongWritable> {
+
 	@Override
 	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+		/*
+		 * input line from file
+		 */
 		String[] line = value.toString().split(";");
+
+		/*
+		 * get date
+		 */
 		String date1 = line[0].substring(0, 10);
+
+		/*
+		 * get customer id
+		 */
 		String custID = line[1].substring(0, 8);
+
+		/*
+		 * get amount
+		 */
 		long amt = Long.parseLong(line[8]);
 
+		/*
+		 * write textpair (date,custid) as key and value will be amount
+		 */
 		context.write(new TextPair(new Text(date1), new Text(custID)), new LongWritable(amt));
 	}
 }
 
+/*
+ * 
+ * param 1 : input key class
+ * 
+ * param 2 : input value class which will be list
+ * 
+ * param 3 : output key class
+ * 
+ * param 4 : output value class
+ * 
+ * 
+ */
 class MaxSpendReducer extends Reducer<TextPair, LongWritable, LongWritable, Text> {
+
 	private TreeMap<Long, String> countMap = new TreeMap<Long, String>();
+
+	/*
+	 * input will be like this
+	 * 
+	 * key : textpair (date,custid)
+	 * 
+	 * value (list) : amount spent
+	 *
+	 */
 
 	@Override
 	public void reduce(TextPair key, Iterable<LongWritable> values, Context context)
 			throws IOException, InterruptedException {
-		long sum = 0;
-		String custID = " ", date1 = " ";
 
+		/*
+		 * make sum of spending
+		 */
+		long sum = 0;
 		for (LongWritable val : values) {
 			sum += val.get();
 		}
-		date1 = key.getFirst().toString().substring(0, 7);
-		custID = key.getSecond().toString();
-		String custIDDate = custID + "\t" + date1;
-		countMap.put(sum, custIDDate);
-		// context.write(new LongWritable(sum), new Text(custIDDate));
+
+		/*
+		 * extract date and custid
+		 * 
+		 * club them in new var
+		 * 
+		 */
+		String date = key.getFirst().toString().substring(0, 7);
+		String custId = key.getSecond().toString();
+		String custId_Date = custId + "\t" + date;
+
+		/*
+		 * make entry of this in countmap
+		 */
+		countMap.put(sum, custId_Date);
+
 	}
 
+	/*
+	 * 
+	 * this method get call when reducer is done with all processing
+	 * 
+	 */
 	@Override
 	public void cleanup(Context context) throws IOException, InterruptedException {
 
+		/*
+		 * get last entry of map as it will have max amount
+		 * 
+		 * 
+		 */
 		long MaxAmount = countMap.pollLastEntry().getKey();
 		String custIdDt = countMap.pollLastEntry().getValue().toString();
+
+		/*
+		 * write to context for output
+		 */
 		context.write(new LongWritable(MaxAmount), new Text(custIdDt));
 
 	}
--- src/main/java/problem/first/CustWithMaxSpendingByMonth/MaxSpendPartitioner.java
+++ src/main/java/problem/first/CustWithMaxSpendingByMonth/MaxSpendPartitioner.java
@@ -3,32 +3,23 @@ package problem.first.CustWithMaxSpendingByMonth;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.Partitioner;
 
+/*
+ * this class get input from mapper and this class will decide where to send this entry i mean which reducer
+ * 
+ * in our example number of reducer is 4
+ * 
+ * 
+ */
 public class MaxSpendPartitioner extends Partitioner<TextPair, LongWritable> {
 
 	@Override
 	public int getPartition(TextPair key, LongWritable val, int numPartitions) {
-		/*
-		 * SimpleDateFormat format1 = new SimpleDateFormat("YYYY");
-		 * SimpleDateFormat format2 = new SimpleDateFormat("MM");
-		 * 
-		 * Date date1=null,month1=null;
-		 */
-		// try
-		// {
-		String date1 = key.getFirst().toString().substring(0, 4);
-		String month1 = key.getFirst().toString().substring(5, 7);
 
-		/*
-		 * } catch(ParseException e) { e.printStackTrace(); }
-		 */
+		String date = key.getFirst().toString().substring(0, 4);
+		String month = key.getFirst().toString().substring(5, 7);
 
-		int hash1 = date1.hashCode();
+		int partition = ((date.hashCode() + month.hashCode()) % numPartitions);
 
-		int hash2 = month1.hashCode();
-
-		int partition = ((hash1 + hash2) % numPartitions);
-
-		// System.out.println("H1:"+hash1+"\tH2:"+hash2+"\tPartitions:"+partition);
 		return partition;
 	}
 
